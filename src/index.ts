fa757e78694421e112a6afbd5a7a0cb98b7ee04a
rca <nem@nem.x0.to>
2024-11-06 20:15:56 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
 import express from 'express';
 import Logger from './logger';
+// import prisma from '@src/prisma';
+import cors from 'cors';
 
-
-
+// Init
 const app = express();
+const DEFAULT_PORT = 3000;
+
+const logger = new Logger();
+logger.setTag('index');
+
+// Startup
+logger.info("サーバーを起動しています...");
+
+const configLogger = logger.getChild('config');
+// Config
+const port = process.env.PORT || DEFAULT_PORT;
+
+// Init server
+app.use(express.json());
+configLogger.info('Express JSON parserを有効にしました');
+
+// For debug only
+const corsOptions = {
+    origin: '*',
+    methos: "GET, POST, PUT, DELETE, OPTIONS",
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+configLogger.warn('全オリジンからのリクエストを許可しました !!for debug only!!');
+
+logger.success('Expressサーバーの初期化が完了しました');
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    logger.success(`サーバーが起動しました。使用ポート: ${port}`);
 });
 
