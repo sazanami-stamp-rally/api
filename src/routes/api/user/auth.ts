d32b73ce2f8144d2f12d76ba213e898a5e773e40
rca <nem@nem.x0.to>
2024-11-07 12:29:06 +0900

    add log

--- src/routes/api/user/auth.ts
+++ src/routes/api/user/auth.ts
@@ -4,6 +4,9 @@ import { activateUser, checkActivated } from '@src/services/user/auth';
 import hasUserId from '@src/routes/middlewares/validation/hasUserId';
 
 const router = Router();
+const logger = new Logger();
+
+logger.setTags(['api', 'user', 'auth']);
 
 router.get('/isActivated', hasUserId, async (req: Request, res: Response) => {
     // hasUserIdミドルウェアを挟んでいる場合, req.userIdは存在することが保証されている
@@ -19,6 +22,7 @@ router.post('/activate', hasUserId, async (req: Request, res: Response) => {
             res.status(400).json({ message: 'User is already activated' });
         } else {
             activateUser(req.userId!).then(() => {
+                logger.info(`ユーザー ${req.userId} がアクティベートされました`);
                 res.status(200).json({ message: 'Successfully activated' });
             });
         }
