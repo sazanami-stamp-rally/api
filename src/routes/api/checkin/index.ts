5b5bb3394cb90eb1db9d4c00b86e0b592a836555
rca <nem@nem.x0.to>
2024-11-07 15:35:44 +0900

    クールダウン関係の処理を修正

--- src/routes/api/checkin/index.ts
+++ src/routes/api/checkin/index.ts
@@ -3,6 +3,7 @@ import Logger from '@src/logger';
 import hasUserId from '@src/routes/middlewares/validation/hasUserId';
 import { handleCheckin, isBoothIdExist } from "@src/services/checkin";
 import { okResponse, notFoundResponse, internalServerErrorResponse } from "@src/models/responses";
+import { canCheckin, setCooldown } from "@src/services/cooldown";
 
 const router = Router();
 const logger = new Logger();
@@ -17,10 +18,24 @@ router.post('/:boothId', hasUserId, async (req, res) => {
         return;
     }
 
+    if (!await canCheckin(req.userId!, boothId)) {
+        logger.info(`クールダウン中のチェックインが試行されました(UID: ${req.userId}, BID: ${boothId})`);
+        res.status(403).json({
+            message: 'クールダウンタイムが終了するまでは再度チェックインできません'
+        });
+        return;
+    }
+
     handleCheckin(req.userId!, boothId).then((checkin) => {
+        setCooldown(req.userId!, boothId).then(() => {
         logger.info(`ユーザー "${checkin.user.display_name}" がブース "${checkin.booth.display_name}" (${checkin.booth.floor}階) にチェックインしました`);
         const resp = okResponse();
         res.status(resp.statusCode).json(resp.body);
+        }).catch((err) => {
+            logger.error(err);
+            const resp = internalServerErrorResponse();
+            res.status(resp.statusCode).json(resp.body);
+        });
     }).catch((err) => {
         logger.error(err);
         const resp = internalServerErrorResponse();
