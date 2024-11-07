462f6c81695cd8de9ae835a986af067950c07a3c
rca <nem@nem.x0.to>
2024-11-07 10:58:24 +0900

    fix

--- src/routes/api/user/authRouter.ts
+++ src/routes/api/user/authRouter.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 import Logger from '@src/logger';
-import { checkActivated } from '@src/services/user/auth';
+import { activateUser, checkActivated } from '@src/services/user/auth';
 import hasUserId from '@src/routes/middlewares/validation/hasUserId';
 
 const router = Router();
@@ -13,11 +13,14 @@ router.get('/isActivated', hasUserId, async (req: Request, res: Response) => {
 });
 
 router.get('/activate', hasUserId, async (req: Request, res: Response) => {
+    // hasUserIdミドルウェアでuserIdがあることが保証されている
     checkActivated(req.headers.userId as string).then((isActivated) => {
         if (isActivated) {
             res.status(400).json({ message: 'User is already activated' });
         } else {
-            res.status(200).json({ message: 'Successfully activated' });
+            activateUser(req.headers.userId as string).then(() => {
+                res.status(200).json({ message: 'Successfully activated' });
+            });
         }
     });
 });
