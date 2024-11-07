866e9a83a1877affcf15e009f067cbf2befbe410
rca <nem@nem.x0.to>
2024-11-07 12:25:19 +0900

    修正

--- src/routes/middlewares/validation/hasUserId.ts
+++ src/routes/middlewares/validation/hasUserId.ts
@@ -1,8 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default function hasUserId(req: Request, res: Response, next: NextFunction) {
-    if (req.headers.userId && typeof req.headers.userId === 'string') {
-        next();
+    if (req.headers.authorization) {
+        const userId = req.headers.authorization.split(' ')[1];
+        if (userId) {
+            req.userId = userId;
+            next();
+        } else {
+            res.status(400).json({ message: 'Missing or invalid user id' });
+        }
     } else {
         res.status(400).json({ message: 'Missing or invalid user id' });
     }
