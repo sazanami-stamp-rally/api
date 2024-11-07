7ae776b7e8171ed43ea09874818b405a609da3bf
rca <nem@nem.x0.to>
2024-11-07 16:21:12 +0900

    wip

---
+++ src/routes/api/common/ranking/index.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import Logger from '@src/logger';
+
+const router = Router();
+const logger = new Logger();
+logger.setTags(['route', 'api', 'common', 'ranking']);
+
+router.get('/', (req, res) => {
+    res.json({
+        message: 'ranking'
+    });
+}
