06e1589e3c2276dc360074936cf22825bf94afd1
rca <nem@nem.x0.to>
2024-11-07 12:25:12 +0900

    コメント修正

--- src/routes/api/user/authRouter.ts
+++ src/routes/api/user/authRouter.ts
@@ -6,19 +6,19 @@ import hasUserId from '@src/routes/middlewares/validation/hasUserId';
 const router = Router();
 
 router.get('/isActivated', hasUserId, async (req: Request, res: Response) => {
-    // hasUserIdミドルウェアでuserIdがあることが保証されている
-    checkActivated(req.headers.userId as string).then((isActivated) => {
+    // hasUserIdミドルウェアを挟んでいる場合, req.userIdは存在することが保証されている
+    checkActivated(req.userId!).then((isActivated) => {
         res.status(200).json({ isActivated });
     });
 });
 
-router.get('/activate', hasUserId, async (req: Request, res: Response) => {
-    // hasUserIdミドルウェアでuserIdがあることが保証されている
-    checkActivated(req.headers.userId as string).then((isActivated) => {
+router.post('/activate', hasUserId, async (req: Request, res: Response) => {
+    // hasUserIdミドルウェアを挟んでいる場合, req.userIdは存在することが保証されている
+    checkActivated(req.userId!).then((isActivated) => {
         if (isActivated) {
             res.status(400).json({ message: 'User is already activated' });
         } else {
-            activateUser(req.headers.userId as string).then(() => {
+            activateUser(req.userId!).then(() => {
                 res.status(200).json({ message: 'Successfully activated' });
             });
         }
