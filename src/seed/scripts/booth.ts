844d41baf3dac77b1bcfff20d197f5aa8835035d
rca <nem@nem.x0.to>
2024-11-10 13:14:44 +0900

    ちゃんと非同期にした

--- src/seed/scripts/booth.ts
+++ src/seed/scripts/booth.ts
@@ -1,28 +1,34 @@
-import csv from 'csv-parser';
 import fs from 'fs';
-import { PrismaClient, Booth } from '@prisma/client';
+import csv from 'csv-parser';
+import { Booth, PrismaClient } from '@prisma/client';
 
-export function seed(rootPath: string): void {
+export async function seed(rootPath: string): Promise<void> {
   console.log('Seeding booths...');
   const prisma = new PrismaClient();
   const data = [] as Booth[];
 
-  fs.createReadStream(`${rootPath}/datas/booth.csv`)
-    .pipe(csv())
-    .on('data', (row) => {
-      data.push({
-        id: row.id as string,
-        display_name: row.display_name as string,
-        floor: row.floor as number,
-        category: row.category as string,
-        flags: row.flags ? row.flags.split(',') : [],
+  await new Promise<void>((resolve, reject) => {
+    fs.createReadStream(`${rootPath}/datas/booth.csv`)
+      .pipe(csv())
+      .on('data', (row) => {
+        data.push({
+          id: row.id as string,
+          display_name: row.display_name as string,
+          floor: row.floor as number,
+          category: row.category as string,
+          flags: row.flags ? row.flags.split(',') : [],
+        });
+      })
+      .on('end', () => {
+        resolve();
+      })
+      .on('error', (err) => {
+        reject(err);
       });
-    })
-    .on('end', async () => {
-      await prisma.booth.createMany({
-        data
-      });
-      console.log('Seeding booths completed!');
-      prisma.$disconnect();
-    });
+  });
+
+  await prisma.booth.createMany({ data });
+  console.log('Seeding booths completed!');
+  await prisma.$disconnect();
 }
+
