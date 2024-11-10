79f9a3f22f999f69f10a6176f1a5e70ea45d92b4
rca <nem@nem.x0.to>
2024-11-10 14:19:57 +0900

    追加

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -1,6 +1,7 @@
 import { seed as booth } from './scripts/booth';
 import { seed as checkpoint } from './scripts/checkpoint';
 import { seed as checkpointBooth } from './scripts/checkpoint_booth';
+import { seed as user } from './scripts/user';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -11,5 +12,6 @@ const rootPath = dirname(fileURLToPath(import.meta.url));
 await booth(rootPath);
 await checkpoint(rootPath);
 await checkpointBooth(rootPath);
+await user(rootPath);
 
 // TODO: 他のシードも実装して呼び出す
