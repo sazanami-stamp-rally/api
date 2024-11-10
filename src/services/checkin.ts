e7633e48e592209801a0176bdbbb67a160bea1de
rca <nem@nem.x0.to>
2024-11-10 10:46:54 +0900

    一部関数修正

--- src/services/checkin.ts
+++ src/services/checkin.ts
@@ -4,28 +4,34 @@ import Logger from '@src/logger';
 const logger = new Logger();
 logger.setTags(['service', 'checkin']);
 
-async function handleCheckin(userId: string, boothId: string) {
-    return await prisma.checkin.create({
-        data: {
-            user_id: userId,
-            booth_id: boothId
-        }, include: { // ログをきれいにするのが主目的なので負荷になるなら消してもいいかも
-            booth: true,
-            user: true
+async function handleCheckin(userId: string, checkpointId: string) {
+  return await prisma.checkin.create({
+    data: {
+      user: {
+        connect: {
+          id: userId
         }
-    }).then((checkin) => {
-        return checkin;
-    });
+      },
+      checkpoint: {
+        connect: {
+          id: checkpointId
+        }
+      }
+    }
+  }).then((checkin) => {
+    logger.info(`User ${userId} checked in at checkpoint ${checkpointId}`);
+    return checkin;
+  });
 }
 
-async function isBoothIdExist(boothId: string) {
-    return await prisma.booth.findUnique({
-        where: {
-            id: boothId
-        }
-    }).then((booth) => {
-        return booth !== null;
-    });
+async function isCheckpointIdExist(checkpointId: string) {
+  return await prisma.checkpoint.findFirst({
+    where: {
+      id: checkpointId
+    }
+  }).then((checkpoint) => {
+    return checkpoint !== null;
+  });
 }
 
-export { handleCheckin, isBoothIdExist };
+export { handleCheckin, isCheckpointIdExist };
