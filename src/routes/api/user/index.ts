8e0385cb8a0b6536fc9d07d91fbb6f6388c62b96
rca <nem@nem.x0.to>
2024-11-10 20:14:48 +0900

    add route

--- src/routes/api/user/index.ts
+++ src/routes/api/user/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "@routes/api/user/auth";
 import profileRouter from "@routes/api/user/profile";
+import historyRouter from "@routes/api/user/history";
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/profile', profileRouter);
+router.use('/history', historyRouter);
 
 export default router;
