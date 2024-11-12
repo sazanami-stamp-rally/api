f4a9c8684ee6339d3cd7ea6aa1c9cddb738b57bb
rca <nem@nem.x0.to>
2024-11-12 14:00:43 +0900

    ページネーションの挙動を修正

--- src/routes/middlewares/pagination.ts
+++ src/routes/middlewares/pagination.ts
@@ -1,14 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default function parsePagination(req: Request, _res: Response, next: NextFunction) {
-  if (!(req.query.page || req.query.from) && req.query.limit) {
+  if (!req.query.limit) {
     req.pagination = undefined;
     next();
   }
 
   const page = parseInt(req.query.page as string, 10) || -1;
   const from = req.query.from as string;
-  const limit = parseInt(req.query.limit as string, 10) || 10;
+  const limit = parseInt(req.query.limit as string, 10) || 5;
 
   req.pagination = { page, from, limit };
 
