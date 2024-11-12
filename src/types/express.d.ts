5f071dc0afd829400fa6730106b25bf2c05bca6d
rca <nem@nem.x0.to>
2024-11-12 10:21:15 +0900

    型定義拡張

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -4,6 +4,11 @@ declare global {
   namespace Express {
     interface Request {
       userId?: string
+      pagination?: {
+        page?: number
+        from?: string
+        limit: number
+      }
     }
   }
 }
