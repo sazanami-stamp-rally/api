bf6f315279ab355742041ac22c112e4b5325a45f
rca <nem@nem.x0.to>
2024-11-12 10:21:08 +0900

    ページネーションを処理するミドルウェアを追加

---
+++ src/routes/middlewares/pagination.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default function parsePagination(req: Request, _res: Response, next: NextFunction) {
+  if (!(req.query.page || req.query.from) && req.query.limit) {
+    next();
+  }
+
+  const page = parseInt(req.query.page as string, 10) || -1;
+  const from = req.query.from as string;
+  const limit = parseInt(req.query.limit as string, 10) || 10;
+
+  req.pagination = { page, from, limit };
+
+  next();
+}
