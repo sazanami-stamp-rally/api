87c4f56663d0cd10059634cbff6c1badf3e996df
rca <nem@nem.x0.to>
2024-11-14 21:46:43 +0900

    インデント

--- src/services/ranking.ts
+++ src/services/ranking.ts
@@ -7,53 +7,53 @@ logger.setTags(['service', 'ranking']);
 
 // 通常のランキング
 async function getUserRanking() {
-    try {
-        const users = await prisma.user.findMany({
-            include: {
-                _count: {
-                    select: { Checkin: true }
-                },
-                Acheivement: true
-            },
-            orderBy: {
-                Checkin: {
-                    _count: 'desc'
-                }
-            }
-        });
-
-        // ユーザーランキングの詰替処理と順位の計算
-        const ranking: UserRankingItem[] = [];
-        let currentRank = 1;
-
-        for (let i = 0; i < users.length; i++) {
-            const user = users[i];
-            const previousUser = i > 0 ? users[i - 1] : null;
-
-            // 同じチェックイン数であれば同順位にする
-            if (previousUser && user._count?.Checkin === previousUser._count?.Checkin) {
-                ranking.push({
-                    rank: ranking[i - 1].rank, // 前のユーザーと同じ順位
-                    userId: user.id,
-                    userName: user.display_name || 'Unknown',
-                    score: user._count?.Checkin || 0
-                });
-            } else {
-                ranking.push({
-                    rank: currentRank,
-                    userId: user.id,
-                    userName: user.display_name || 'Unknown',
-                    score: user._count?.Checkin || 0
-                });
-            }
-
-            // 順位を次に進める
-            currentRank = ranking[i].rank + 1;
+  try {
+    const users = await prisma.user.findMany({
+      include: {
+        _count: {
+          select: { Checkin: true }
+        },
+        Acheivement: true
+      },
+      orderBy: {
+        Checkin: {
+          _count: 'desc'
         }
-        return ranking;
-    } catch (error) {
-        throw error;
+      }
+    });
+
+    // ユーザーランキングの詰替処理と順位の計算
+    const ranking: UserRankingItem[] = [];
+    let currentRank = 1;
+
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+      const previousUser = i > 0 ? users[i - 1] : null;
+
+      // 同じチェックイン数であれば同順位にする
+      if (previousUser && user._count?.Checkin === previousUser._count?.Checkin) {
+        ranking.push({
+          rank: ranking[i - 1].rank, // 前のユーザーと同じ順位
+          userId: user.id,
+          userName: user.display_name || 'Unknown',
+          score: user._count?.Checkin || 0
+        });
+      } else {
+        ranking.push({
+          rank: currentRank,
+          userId: user.id,
+          userName: user.display_name || 'Unknown',
+          score: user._count?.Checkin || 0
+        });
+      }
+
+      // 順位を次に進める
+      currentRank = ranking[i].rank + 1;
     }
+    return ranking;
+  } catch (error) {
+    throw error;
+  }
 }
 
 // TODO: 複雑な要件を持つランキングを算出できるようにする
