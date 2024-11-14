6fdb3ba515658da5e7d0c71247c2f6e991ae957d
rca <nem@nem.x0.to>
2024-11-14 19:45:43 +0900

    キャッシュ取得を実装

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -2,9 +2,22 @@ import redis from "@src/ioredis";
 import { getUserRanking } from "@src/services/ranking";
 
 async function updateUserRankingCache() {
-    getUserRanking().then((ranking) => {
-        redis.set('user-ranking', JSON.stringify(ranking));
-    });
+  getUserRanking().then((ranking) => {
+    redis.set('user-ranking', JSON.stringify(ranking));
+  });
 }
 
-export { updateUserRankingCache };
+
+async function getUserRankingCache() {
+  const ranking = await redis.get('user-ranking');
+  
+  if (ranking === null) {
+    await updateUserRankingCache();
+    return await getUserRankingCache();
+  }
+
+  return JSON.parse(ranking);
+}
+
+
+export { updateUserRankingCache, getUserRankingCache }
