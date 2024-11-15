940bcb895ebce0909e379d318a29b73391e4ce8c
rca <nem@nem.x0.to>
2024-11-15 23:44:31 +0900

    service作成 wip

--- src/services/user/profile.ts
+++ src/services/user/profile.ts
@@ -5,26 +5,72 @@ const logger = new Logger();
 logger.setTags(['service', 'user', 'profile']);
 
 async function setUserDisplayName(userId: string, displayName: string) {
-    return await prisma.user.update({
-        where: {
-            id: userId
-        },
-        data: {
-            display_name: displayName
-        }
-    }).then((user) => {
-        return user;
-    });
+  return await prisma.user.update({
+    where: {
+      id: userId
+    },
+    data: {
+      display_name: displayName
+    }
+  }).then((user) => {
+    return user;
+  });
 }
 
 async function getUserDisplayName(userId: string) {
-    return await prisma.user.findUnique({
-        where: {
-            id: userId
-        }
-    }).then((user) => {
-        return user?.display_name;
-    });
+  return await prisma.user.findUnique({
+    where: {
+      id: userId
+    }
+  }).then((user) => {
+    return user?.display_name;
+  });
+}
+
+async function setUserAllowShowOnSignage(userId: string, allowShowOnSignage: boolean) {
+  return await prisma.user.update({
+    where: {
+      id: userId
+    },
+    data: {
+      allow_show_on_signage: allowShowOnSignage
+    }
+  }).then((user) => {
+    return user;
+  });
+}
+
+async function setUserAllowUseInBooth(userId: string, allowUseInBooth: boolean) {
+  return await prisma.user.update({
+    where: {
+      id: userId
+    },
+    data: {
+      allow_use_in_booth: allowUseInBooth
+    }
+  }).then((user) => {
+    return user;
+  });
+}
+
+async function getAllowShowOnSignage(userId: string) {
+  return await prisma.user.findUnique({
+    where: {
+      id: userId
+    }
+  }).then((user) => {
+    return user?.allow_show_on_signage
+  });
+}
+
+async function getAllowUseInBooth(userId: string) {
+  return await prisma.user.findUnique({
+    where: {
+      id: userId
+    }
+  }).then((user) => {
+    return user?.allow_use_in_booth
+  });
 }
 
 export { setUserDisplayName, getUserDisplayName };
