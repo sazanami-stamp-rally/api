e2a80a8454b52903408341604bd74c246258ae1e
rca <nem@nem.x0.to>
2024-11-16 10:54:00 +0900

    add seed

--- scripts/seed/index.ts
+++ scripts/seed/index.ts
@@ -2,6 +2,7 @@ import { seed as booth } from './scripts/booth';
 import { seed as checkpoint } from './scripts/checkpoint';
 import { seed as checkpointBooth } from './scripts/checkpoint_booth';
 import { seed as user } from './scripts/user';
+import { seed as user1 } from './scripts/user_1';
 import { seed as seeded } from './scripts/seeded';
 
 import { isDbSeeded } from './isDbSeeded';
@@ -20,6 +21,7 @@ await booth(rootPath);
 await checkpoint(rootPath);
 await checkpointBooth(rootPath);
 await user(rootPath);
+await user1(rootPath);
 
 // シード済みであることを記録する
 await seeded();
